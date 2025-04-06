4da02ba0db89cc1988bfa7fc287e14307a73e301
YarthiasY <cordero.mathias.2006@gmail.com>
2025-04-06 23:14:47 +0200

    V2

--- creat_map.c
+++ creat_map.c
@@ -26,21 +26,15 @@ void creat_map(){
 
     srand(time(NULL));
     START=(SIZE/4)+rand()%(SIZE/2);
-    END=(SIZE/4)+rand()%(SIZE/2);
     printf("\n%d %d\n",START, END);
     path=malloc(sizeof(int) * SIZE );
     if (path==NULL){
         printf("erreur 3");
        exit(3);
     }
-    if (SIDE=='d'){
-        tmp=END;
-        END=START;
-        START=tmp;
-    }
     if (SIDE=='u' || SIDE=='d'){
         while (n<SIZE+1){
-           *(path+count)= rand()%3;
+           *(path+count)= rand()%7;
            if (*(path+count)==1) {
                n++;
            }
@@ -60,7 +54,7 @@ void creat_map(){
     for (int i = 10; i < SIZE+1; i++){
         printf("%d  ",i);
     }
-    for (int i = 0; i < SIZE-floor(SIZE/3); i++){
+    for (int i = 0; i < SIZE-1; i++){
     k=0;
      printf("\n%d",i+1);
      for (int j = 1; j < SIZE+1; j++){
@@ -73,17 +67,41 @@ void creat_map(){
             l=j;
             k=1;
         }
-        else if (*(path+i)==1 && k==0 && j==l-2 && *(path+i-1)!=2 && l-(SIZE/10)>2){
+        else if (*(path+i)==1 && j==l-2 && *(path+i-1)!=2  && *(path+i-1)!=4 && k==0 && l-(SIZE/10)>2){
             printf("\033[48;2;153;87;1m            \033[0m");
             l = j;
             k = 1;
             j+=2;
         }        
-        else if (*(path+i)==2 && j==l && *(path+i-1)!=1 && k==0 && l+(SIZE/10)<SIZE){
+        else if (*(path+i)==2 && j==l && *(path+i-1)!=1 && *(path+i-1)!=3 && *(path+i-1)!=5 && k==0 && l+(SIZE/10)<SIZE-2){
             printf("\033[48;2;153;87;1m            \033[0m");
             l=j+2;
             k=1;
             j+=2;
+        }
+        else if (*(path+i)==3 && j==l-1 && *(path+i-1)!=2 && *(path+i-1)!=4 && *(path+i-1)!=6 && k==0 && l-(SIZE/10)>1){
+            printf("\033[48;2;153;87;1m        \033[0m");
+            l = j;
+            k = 1;
+            j+=1;
+        }        
+        else if (*(path+i)==4 && j==l && *(path+i-1)!=1 && *(path+i-1)!=3 && *(path+i-1)!=5 &&  k==0 && l+(SIZE/10)<SIZE-1){
+            printf("\033[48;2;153;87;1m        \033[0m");
+            l=j+1;
+            k=1;
+            j+=1;
+        }
+        else if (*(path+i)==5 && j==l-3 && *(path+i-1)!=2 && *(path+i-1)!=4 && *(path+i-1)!=6 && k==0 && l-(SIZE/10)>3){
+            printf("\033[48;2;153;87;1m                \033[0m");
+            l = j;
+            k = 1;
+            j+=3;
+        }        
+        else if (*(path+i)==6 && j==l && *(path+i-1)!=1 && *(path+i-1)!=3 && *(path+i-1)!=5 &&  k==0 && l+(SIZE/10)<SIZE-3){
+            printf("\033[48;2;153;87;1m                \033[0m");
+            l=j+3;
+            k=1;
+            j+=3;
         }
         else if (j==l && k==0){
             printf("\033[48;2;153;87;1m    \033[0m");
@@ -93,60 +111,7 @@ void creat_map(){
         else{
             printf("\033[48;2;11;102;11m    \033[0m");
         }
-     } 
-     printf("  %d  ",*(path+i));
-    }
-    for (int i = SIZE-floor(SIZE/3); i < SIZE-1; i++){
-       k=0;
-       printf("\n%d",i+1);
-        for (int j = 1; j < SIZE+1; j++){
-            if((START%2)!=(END%2) && d==0){
-                if (l>END && *(path+i-1)!=2 && j==l-2){
-                    printf("\033[48;2;11;102;11m    \033[0m");
-                    printf("\033[48;2;153;87;1m        \033[0m");
-                    l=j+1;
-                    k=1;
-                    j+=3;
-                    *(path+i)=3; 
-                    d=1;
-                }
-                else if (l<END && *(path+i-1)!=1 && j==l){
-                    printf("\033[48;2;153;87;1m        \033[0m");
-                    l=j+1;
-                    k=1;
-                    j+=2; 
-                    *(path+i)=4;
-                    d=1;
-                }    
-            }
-            if (l>END && *(path+i-1)!=2 && *(path+i-1)!=4 && j==l-2 && k==0){
-                printf("\033[48;2;153;87;1m            \033[0m");
-                l=j;
-                k=1;
-                j+=2;
-                *(path+i)=1;
-            }
-            else if (l<END && *(path+i-1)!=1 && *(path+i-1)!=3 && j==l && k==0){
-                printf("\033[48;2;153;87;1m            \033[0m");
-                l=j+2;
-                k=1;
-                j+=2;
-                *(path+i)=2;
-            }
-            else if (j==l && k==0){
-                printf("\033[48;2;153;87;1m    \033[0m");
-                *(path+i)=0;
-                k=1;
-            }
-            else{
-                printf("\033[48;2;11;102;11m    \033[0m");
-            }
-        }
-    printf("  %d  ",*(path+i));
-    }
-    if (l!=END){
-        printf("erreur 4");
-        exit(4);
+    } 
     }
     printf ("\n%d", SIZE);
     for (int j = 1; j < SIZE+1; j++){
@@ -157,7 +122,9 @@ void creat_map(){
             printf("\033[48;2;11;102;11m    \033[0m");
         }
     }
-    }
+    } 
+    END=l;
 }
+
 
 //build the first map with the data
