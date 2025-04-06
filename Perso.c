1d5ffdb31b3a25f17e8f8e2e5d60b24f1c7bd714
Alexandre-bat <alexandre.bernard9@etu.cyu.fr>
2025-04-06 18:24:15 +0200

    E2

--- Perso.c
+++ Perso.c
@@ -6,12 +6,9 @@
 //Define icones
 typedef struct{
   int dmg;
-  int hp;
   int mana;
   int speed;
   int crit;
-  int dodge;
-  int heal;
   int level;
   int exp;
   int type;
@@ -22,14 +19,9 @@ typedef struct{
 typedef struct{
   int dmg;
   int hp;
-  int mana;
   int speed;
-  int crit;
   int dodge;
-  int heal;
   int level;
-  int exp;
-  int carry;
 }Attacker;
 
 // Function to create a new defender
@@ -44,33 +36,32 @@ Defender* create_defender() {
     }
 
     new_defender->dmg = rand()%10 + 1;
-    new_defender->hp = rand()%10 + 10;
+
     new_defender->mana = 0;
     new_defender->speed = rand()%10 + 1;
     new_defender->crit = rand()%10 + 1;
-    new_defender->dodge = rand()%2;
-    new_defender->heal = 100;
     new_defender->level = 1;
     new_defender->exp = 0;
-    printf("Quel type de défenseur voulez-vous ?\n");
-    printf("Choissisez le type de défenseur de type \n");
-    printf("0. Défenseur de type càc\n");
-    printf("1. Défenseur de type distance\n");
-    while((scanf("%d", &new_defender->type) != 0 || "%d", &new_defender->type) != 1) {
+    printf("Select type of defender?\n");
+    printf("Choissisez le type de defenseur de type \n");
+    printf("0. Type of defender is melee\n");
+    printf("1. Type of defender is range\n");
+    do{
+    scanf("%d", &new_defender->type);
     if(new_defender->type == 0) {
-        printf("Vous avez choisi un défenseur de type càc\n");
+        printf("You choose a melee\n");
         new_defender->carry = 1;
 
       } else if(new_defender->type == 1) {
-          printf("Vous avez choisi un défenseur de type distance\n");
+          printf("You choose a range\n");
           new_defender->carry = rand()%5 + 3;
       }
       else{
-          printf("Type de défenseur invalide. Veuillez réessayer.\n");
+          printf("Type of defender is invalide. Try again.\n");
           continue; // Continue the loop if the input is invalid
       }
-  }
-    return new_defender;
+  }while(new_defender->type != 0 && new_defender->type != 1);
+  return new_defender;
 }
 
 // Function to create a new attacker
@@ -86,14 +77,31 @@ Attacker* create_attacker(){
 
     new_attacker->dmg = rand()%10 + 1;
     new_attacker->hp = rand()%10 + 10;
-    new_attacker->mana = 0;
     new_attacker->speed = rand()%10 + 1;
-    new_attacker->crit = rand()%10 + 1;
     new_attacker->dodge = rand()%2;
-    new_attacker->heal = 100;
-    new_attacker->level = 1;
-    new_attacker->exp = 0;
+    new_attacker->dodge = 1;
     return new_attacker;
+}
+
+void show_defender(Defender* defender){
+    printf("Defender stats:\n");
+    printf("  Damage: %d\n", defender->dmg);
+    printf("  Mana: %d\n", defender->mana);
+    printf("  Speed: %d\n", defender->speed);
+    printf("  Critical Hit Chance: %d\n", defender->crit);
+    printf("  Level: %d\n", defender->level);
+    printf("  Experience: %d\n", defender->exp);
+    printf("  Type: %s\n", (defender->type == 0) ? "Melee" : "Range");
+    printf("  Carry: %d\n", defender->carry);
+}
+
+void show_attacker(Attacker* attacker){
+    printf("Attacker stats:\n");
+    printf("  Damage: %d\n", attacker->dmg);
+    printf("  HP: %d\n", attacker->hp);
+    printf("  Speed: %d\n", attacker->speed);
+    printf("  Dodge Chance: %d\n", attacker->dodge);
+    printf("  Level: %d\n", attacker->level);
 }
 
 
@@ -104,11 +112,13 @@ Attacker* create_attacker(){
 int main(){
   srand(time(NULL)); 
   Defender* defender = create_defender();
+  show_defender(defender);
   Attacker* attacker = create_attacker();
+  show_attacker(attacker);
 
-  printf("hello world\n");
-  // Test1
-  //  Test2
-  printf("bonjour\n");
+  // Free the allocated memory
+  free(defender);
+  free(attacker);
+  return 0;
 }
 
