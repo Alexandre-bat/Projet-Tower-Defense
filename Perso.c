a91d1cb6e2bc2881f05c5671e3753651d1d0a912
Alexandre-bat <alexandre.bernard9@etu.cyu.fr>
2025-04-10 11:39:16 +0200

    Update Perso.c

--- Perso.c
+++ Perso.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h> // For strcpy
-#include <locale.h> // For UTF-8 support
 #include<math.h>
 #include<time.h>
 
@@ -10,10 +9,7 @@ typedef struct{
   char skin[50];
   int dmg;
   int mana;
-  int speed;
-  int crit;
   int level;
-  int exp;
   int type;
   int carry;
 }Defender;
@@ -23,8 +19,6 @@ typedef struct{
   char skin[50];
   int dmg;
   int hp;
-  int speed;
-  int dodge;
   int level;
 }Attacker;
 
@@ -51,13 +45,9 @@ Defender* create_defender() {
         return NULL;
     }
 
-    strcpy(new_defender->skin, "\xF0\x9F\x9A\xB9"); // Example skin, you can change it to any other skin you want
-    new_defender->dmg = rand()%10 + 1;
+    strcpy(new_defender->skin, "\xE2\x9D\x95"); // Example skin, you can change it to any other skin you want
     new_defender->mana = 0;
-    new_defender->speed = rand()%10 + 1;
-    new_defender->crit = rand()%10 + 1;
     new_defender->level = 1;
-    new_defender->exp = 0;
 
     printf("Select type of defender?\n");
     printf("Choissisez le type de defenseur de type \n");
@@ -65,24 +55,27 @@ Defender* create_defender() {
     printf("1. Type of defender is range\n");
 
     while (1) {
-      //Not character input
+    //Char input
       if (scanf("%d", &new_defender->type) != 1) {
           printf("Invalid input. Please enter a number.\n");
           while(getchar() != '\n'); // Clear the input buffer
           new_defender->type = -1; // Set an invalid value to ensure the loop continues
           continue;
       }
+      //Int input
       if (new_defender->type == 0) {
           printf("You chose Melee defender.\n");
           new_defender->carry = 1;
+          new_defender->dmg = 5;
           break;
       } else if (new_defender->type == 1) {
           printf("You chose Range defender.\n");
-          new_defender->carry = rand() % 5 + 3;
+          new_defender->dmg = 2;
           break;
       } else {
           printf("Invalid defender type. Try again.\n");
           while(getchar() != '\n');
+          continue;
       }
   }
   return new_defender;
@@ -99,37 +92,33 @@ Attacker* create_attacker(){
         return NULL;
     }
 
-    strcpy(new_attacker->skin,"\xF0\x9F\x9A\xBA");
-    new_attacker->dmg = rand()%10 + 1;
-    new_attacker->hp = rand()%10 + 10;
-    new_attacker->speed = rand()%10 + 1;
-    new_attacker->dodge = rand()%2;
+    strcpy(new_attacker->skin,"\xE2\x9D\x8C");
+    new_attacker->dmg = 1;
+    new_attacker->hp = 10;
     new_attacker->level = 1;
     return new_attacker;
 }
 
-void show_defender(Defender* defender){
-    printf("Defender stats:\n");
-    printf("  Skin: %s\n", defender->skin);
-    printf("  Damage: %d\n", defender->dmg);
-    printf("  Mana: %d\n", defender->mana);
-    printf("  Speed: %d\n", defender->speed);
-    printf("  Critical Hit Chance: %d\n", defender->crit);
-    printf("  Level: %d\n", defender->level);
-    printf("  Experience: %d\n", defender->exp);
-    printf("  Type: %s\n", (defender->type == 0) ? "Melee" : "Range");
-    printf("  Carry: %d\n", defender->carry);
+void MakeWave(int wave, int Size, Attacker* t, int x){
+	int coef = Size*wave;
+	Attacker* tab =(Attacker*)malloc(coef*sizeof(Attacker));
+	
+	for(int i = 0; i<coef; i++){
+		*(tab+i) = *(create_attacker());
+	}
+	t = tab;
 }
 
-void show_attacker(Attacker* attacker){
-    printf("Attacker stats:\n");
-    printf("  Skin: %s\n", attacker->skin);
-    printf("  Damage: %d\n", attacker->dmg);
-    printf("  HP: %d\n", attacker->hp);
-    printf("  Speed: %d\n", attacker->speed);
-    printf("  Dodge Chance: %d\n", attacker->dodge);
-    printf("  Level: %d\n", attacker->level);
+void MakeMoney(int* money, Attacker* t, int coef){
+	
+	for(int i = 0; i<coef; i++){
+		if((t+i)->hp <= 0){
+			money = money + t[i].level;
+			//Free the space when the Attacker is dead
+		}
+	}
 }
+
 
 
 
@@ -137,21 +126,30 @@ void show_attacker(Attacker* attacker){
 
 
 int main(){
-  setlocale(LC_ALL, ""); // Set the locale to support UTF-8 characters
   srand(time(NULL));
+  int money = 0;
+  int wave = 0;
+  Attacker* TabAtt;
+  int coef;
+  int SizeMap;
+  printf("What is the size of the map?\n");
+  scanf("%d",&SizeMap);
 
   Defender* defender = create_defender();
-  show_defender(defender);
   //create a new visualisation for each lvl with money
 
   printf("\n\n");
 
   Attacker* attacker = create_attacker();
-  show_attacker(attacker);
-  //create a new visualisation for each lvl
-
+  //create a new visualisation for each lvl with money
+  
+  MakeWave(wave,SizeMap, TabAtt, coef);
+  MakeMoney(&money,TabAtt,coef);
+  
+  
+  
+  free(TabAtt);	
   free(defender);
   free(attacker);
   return 0;
 }
-
