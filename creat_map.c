1eec25914de0f50618717794c6f334c6adb18902
YarthiasY <cordero.mathias.2006@gmail.com>
2025-05-02 11:46:00 +0200

    Add files via upload
    add 2/3 ; up/down

--- creat_map.c
+++ creat_map.c
@@ -1,127 +1,88 @@
-#include "all.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
 
-void creat_map(){
-    int SIZE, START, END, count=0, n=0,k=0,d=0,l,tmp;
-    char SIDE;
-    int* path;
-    printf("What size do you want for your map ?\n 11-99\n");
-    scanf("%d",&SIZE);
-    if (SIZE<11 || SIZE>99) {
-        printf("erreur 1");
+int** generatePath(int* s) {
+    int size;
+    printf("What is the size of the map (11 - 99)? : ");
+    scanf("%d", &size);
+    if (size < 11 || size > 99) {
+        printf("Size must be between 11 and 99.\n");
         exit(1);
     }
-    printf("In which side is the start on the map ?\n u/d/r/l\n");
-    scanf(" %c",&SIDE);
-    if ((SIDE!= 'u') && (SIDE!= 'd') && (SIDE!= 'r') && (SIDE!= 'l')){
-        printf("erreur 2");
-        exit(2);
-    }
-
-
-    // get the size and the side for build the map
-
-
+    *s = size;
     srand(time(NULL));
-    START=(SIZE/4)+rand()%(SIZE/2);
-    printf("\n%d %d\n",START, END);
-    path=malloc(sizeof(int) * SIZE );
-    if (path==NULL){
-        printf("erreur 3");
-       exit(3);
-    }
-    if (SIDE=='u' || SIDE=='d'){
-        while (n<SIZE+1){
-           *(path+count)= rand()%7;
-           if (*(path+count)==1) {
-               n++;
-           }
-           printf("  %d\n",*(path+count));
-           count++;
-        }    
     
+    int** grid = malloc(size * sizeof(int*));
+    if (grid == NULL) {
+        printf("Erreur d'allocation mémoire.\n");
+        exit(1);
+    }
 
+    for (int i = 0; i < size; i++) {
+        grid[i] = malloc(size * sizeof(int));
+        if (grid[i] == NULL) {
+            printf("Erreur d'allocation mémoire pour la ligne %d.\n", i);
+            exit(1);
+        }
+    }
+
+    int currentRow = rand() % (size/3) +size/3;  // S'assurer que currentRow est dans les bornes
+    int lastDirection = 0;
+    int pastDirection = 0; // 0: tout droit, 1: haut, 2: bas
+
+    // Initialisation de la grille avec des espaces vides
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            grid[i][j] = 0;
+        }
+    }
     
-    //generate a random path 
+    int col = 1;
+    int direction;
+    grid[currentRow][0] = 1;
     
-    
-    printf("O  ");
-    for (int i = 1; i < 10; i++){
-        printf("%d   ",i);
-    }
-    for (int i = 10; i < SIZE+1; i++){
-        printf("%d  ",i);
-    }
-    for (int i = 0; i < SIZE-1; i++){
-    k=0;
-     printf("\n%d",i+1);
-     for (int j = 1; j < SIZE+1; j++){
-        if (i<9 && j==1) {
-            printf(" ");
-         }
-        if (j==START && i==0){
-            printf("\033[48;2;153;87;1m    \033[0m"); 
-            *(path+i)=0;   
-            l=j;
-            k=1;
-        }
-        else if (*(path+i)==1 && j==l-2 && *(path+i-1)!=2  && *(path+i-1)!=4 && k==0 && l-(SIZE/10)>2){
-            printf("\033[48;2;153;87;1m            \033[0m");
-            l = j;
-            k = 1;
-            j+=2;
-        }        
-        else if (*(path+i)==2 && j==l && *(path+i-1)!=1 && *(path+i-1)!=3 && *(path+i-1)!=5 && k==0 && l+(SIZE/10)<SIZE-2){
-            printf("\033[48;2;153;87;1m            \033[0m");
-            l=j+2;
-            k=1;
-            j+=2;
-        }
-        else if (*(path+i)==3 && j==l-1 && *(path+i-1)!=2 && *(path+i-1)!=4 && *(path+i-1)!=6 && k==0 && l-(SIZE/10)>1){
-            printf("\033[48;2;153;87;1m        \033[0m");
-            l = j;
-            k = 1;
-            j+=1;
-        }        
-        else if (*(path+i)==4 && j==l && *(path+i-1)!=1 && *(path+i-1)!=3 && *(path+i-1)!=5 &&  k==0 && l+(SIZE/10)<SIZE-1){
-            printf("\033[48;2;153;87;1m        \033[0m");
-            l=j+1;
-            k=1;
-            j+=1;
-        }
-        else if (*(path+i)==5 && j==l-3 && *(path+i-1)!=2 && *(path+i-1)!=4 && *(path+i-1)!=6 && k==0 && l-(SIZE/10)>3){
-            printf("\033[48;2;153;87;1m                \033[0m");
-            l = j;
-            k = 1;
-            j+=3;
-        }        
-        else if (*(path+i)==6 && j==l && *(path+i-1)!=1 && *(path+i-1)!=3 && *(path+i-1)!=5 &&  k==0 && l+(SIZE/10)<SIZE-3){
-            printf("\033[48;2;153;87;1m                \033[0m");
-            l=j+3;
-            k=1;
-            j+=3;
-        }
-        else if (j==l && k==0){
-            printf("\033[48;2;153;87;1m    \033[0m");
-            *(path+i)=0;
-            k=1;
-        }
-        else{
-            printf("\033[48;2;11;102;11m    \033[0m");
-        }
-    } 
-    }
-    printf ("\n%d", SIZE);
-    for (int j = 1; j < SIZE+1; j++){
-        if (j==l){
-            printf("\033[48;2;153;87;1m    \033[0m");
-        }
-        else {
-            printf("\033[48;2;11;102;11m    \033[0m");
+    while (col != 0) {
+        direction = rand() % 3; // Réduire la fréquence des virages
+
+        if (direction == 1 && currentRow > 3 && pastDirection!=2 && lastDirection != 2) {  // Vérification des bornes
+            currentRow--;
+            grid[currentRow][col] = 1;
+            lastDirection = 1;
+            pastDirection=1;
+        } else if (direction == 2 && currentRow < size - 3 && pastDirection!=1 && lastDirection != 1) {
+            currentRow++;
+            grid[currentRow][col] = 1;
+            lastDirection = 2;
+            pastDirection=1;
+        } else {
+        		if(lastDirection == 0){
+        		pastDirection=0;
+        		}
+            col++;
+            lastDirection == 0;
+            if (col == size) {
+                col = 0;
+            }
+            grid[currentRow][col] = 1;
+            lastDirection = 0;
         }
     }
-    } 
-    END=l;
+
+    // Position du début et de la fin
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (grid[i][j] == 1 && j == size - 1) {
+                grid[i][j] = 100;  // Fin du chemin
+            }
+            if (grid[i][j] == 1 && j == 0) {
+                grid[i][j] = 101;  // Début du chemin
+            }
+        }
+    }
+
+    return grid;
 }
 
 
-//build the first map with the data
